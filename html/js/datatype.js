a3ef1e7d79b956985af111102e2c26d9b7902123
navinsivakumar05 <navinsivakumar5@gmail.com>
2025-03-05 18:50:57 +0530

    javascript files

---
+++ html/js/datatype.js
@@ -0,0 +1,25 @@
+// // var age = 25.3;
+// // var name = 'navin';
+// // is_married = false;
+// // console.log(typeof age);
+// // console.log(typeof name);
+// // console.log(typeof is_married);
+
+
+// const s1 = Symbol();
+// console.log(s1);
+// const s2 = Symbol();
+// console.log(s2);
+// console.log(s1 == s2);
+
+// var courses = ['c', 'c++', 'java'];
+// console.log(typeof courses);
+// var student = {
+//     'name': 'Navin',
+//     'age': 20
+// };
+// console.log(typeof student);
+
+// var time = new Date();
+// console.log(time);
+// console.log(typeof time);
