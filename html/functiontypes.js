4a5b4d27756bb9da916474afadd63339a65c7500
navinsivakumar05 <navinsivakumar5@gmail.com>
2025-04-15 13:01:27 +0530

    modified in 15.04.2025

---
+++ html/functiontypes.js
@@ -0,0 +1,20 @@
+
+
+function fname(uname) {
+    console.log("i am a function" + uname);
+}
+fname(' Naivn');
+
+//ANANAMOUS FUNCTION
+
+let var2 = function () {
+    console.log('i am ananamous function');
+}
+var2();
+
+//ARROW FUNCTION
+
+let fatarrow = () => {
+    console.log("i am arrow function");
+}
+fatarrow();
